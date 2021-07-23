0fac040eb27b1e035394f757b8be725f3288f40d
Jeremy Honl <jerhon@users.noreply.github.com>
2021-07-22 22:29:38 -0500

    Correct Content-Type for application/json.

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-/*************************************************************************
- * Trying out Go, this is a simple greeting HTTP microservice.
- * Make a request to "/greeting" and it returns the customary greeting.
- *************************************************************************/
 package main
 
 import (
@@ -14,24 +10,24 @@ type GreetingDto struct {
 	Greeting string `json:"greeting"`
 }
 
-func handleGetGreeting(writer http.ResponseWriter, request *http.Request) {
-	writer.Header().Add("content","application/json")
+func handleHello(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "application/json")
 	err := json.NewEncoder(writer).Encode(GreetingDto{Greeting: "Hello, world!"})
 	if err != nil {
 		return
 	}
 }
 
-func greetingRoutes() *mux.Router {
+func getRoutes() *mux.Router {
 	r := mux.NewRouter()
-	r.Path("/greeting").Methods("GET").HandlerFunc( handleGetGreeting )
+	r.Path("/greeting").Methods("GET").HandlerFunc(handleHello)
 	return r
 }
 
 func main() {
-	r := greetingRoutes()
+	r := getRoutes()
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
-		return 
+		return
 	}
 }
